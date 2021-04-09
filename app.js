463d28c63ca0ea237869c60d2fe3de578585c743
Shawn Wood <Shawnwood97@gmail.com>
2021-04-09 02:08:43 -0600

    finished git portion, all posts displaying on page load.

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function patchIt() {
       console.log(this.responseText);
       // Also return the PATCHED conttent on the page, not formatted well, starting to
       // run into time constraints.
-      patchResponse.innerText = `${JSON.stringify(patchInfo)}`;
+      patchResponse.innerText = `Post Updated!`;
     }
   };
   ajax.open("PATCH", "https://jsonplaceholder.typicode.com/posts/1", true);
@@ -74,7 +74,8 @@ let patchBtn = document.getElementById("patchBtn");
 patchBtn.addEventListener("click", patchIt);
 
 // DELETE
-
+// I over thought this wayyyyyyyyy too much.
+// Function to delete the first object in posts
 function delIt() {
   let ajax = new XMLHttpRequest();
   ajax.onreadystatechange = function () {
@@ -82,12 +83,43 @@ function delIt() {
       // This returned 200 for a status
       // console.log(this.status);
       console.log(this.responseText);
+
+      delResponse.innerText = "Post Deleted";
     }
   };
   ajax.open("DELETE", "https://jsonplaceholder.typicode.com/posts/1", true);
   ajax.send();
 }
 
-let delUserId = document.getElementById("delPost");
+let delPost = document.getElementById("delPost");
+let delResponse = document.getElementById("deleteResponse");
 
-delUserId.addEventListener("click", delIt);
+delPost.addEventListener("click", delIt);
+
+// Get posts, not a function so it happens on page load.
+let ajax = new XMLHttpRequest();
+ajax.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    // This returned 200 for a status
+    // console.log(this.status);
+    // console.log(this.responseText);
+
+    // get posts as an object
+    let allPosts = JSON.parse(this.responseText);
+    // log posts
+    // console.log(allPosts);
+
+    // forloop to isolate each object and place each item in a variable and add each post usi innerHTML.
+    for (let i = 0; i < allPosts.length; i++) {
+      let postBody = allPosts[i].body;
+      let postTitle = allPosts[i].title;
+      let postUser = allPosts[i].id;
+
+      postsWrap.innerHTML += `<div style="border: 2px solid black; margin-bottom: 5px;"> <h2> ${postTitle}</h2> <p>${postBody}</p> By ID: ${postUser} </div>`;
+    }
+  }
+};
+ajax.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+ajax.send();
+
+let postsWrap = document.getElementById("posts");
